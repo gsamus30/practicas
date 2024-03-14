b6add7626fbc7c7c62305b85a3a4ea7d7f0273c7
Manuelfc08 <mfernandezc22.fpcoruna.afundacion.org>
2024-03-14 12:59:18 +0100

    Segundo paso - Construyendo

--- manuel/java/HundirLaFlota/src/Main.java
+++ manuel/java/HundirLaFlota/src/Main.java
@@ -2,14 +2,33 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int ejemplo=7;
+        int [] lista={15,39,-200,50,10,78,88888};
+        char [] [] tablero = {{'.','.','.','.','.','.'},
+                {'.','.','.','.','.','.'},{'.','.','.','.','.','.'},
+                {'.','.','.','.','.','.'},{'.','.','.','.','.','.'},
+                {'.','.','.','.','.','.'}};
+        //lista= new int [6];
+        /*
+        * 1.Darle Valores
+        * 2.Imprimir valores
+        * 3.Declarar Tablero
+        * 4.Darle Valores
+        * 5.Imprimirlo
+        * Home pintar barcos, pintar bordes
+        * */
+        int i=0;
+        while(i<lista.length){
+            System.out.print(lista[i]+" ");
+            i++;
         }
+        for (int j=0; j<tablero.length;j++){
+            System.out.println();
+            for (int t=0; t< tablero[j].length;t++){
+                System.out.print(tablero[j][t]+"  ");
+            }
+        }
+
     }
+
 }
