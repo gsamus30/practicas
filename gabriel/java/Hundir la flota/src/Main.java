e9ac4219d423ef069f15559da86f8ae17347db69
Gabriel <gsamus30@gmail.com>
2024-03-14 12:59:44 +0100

    Construyendo el tablero

--- gabriel/java/Hundir la flota/src/Main.java
+++ gabriel/java/Hundir la flota/src/Main.java
@@ -2,14 +2,32 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int ejemplo = 7;
+        //lista = new int [6];
+        // 1)Darle valores [ | | | | | ]
+        int [] lista= {0,0,1,0,0,0};
+        char [][] tablero = {{'.','.','.','.','.','.'}
+                            ,{'.','.','.','.','.','.'}
+                            ,{'.','.','.','.','.','.'}
+                            ,{'.','.','.','.','.','.'}
+                            ,{'.','.','.','.','.','.'}
+                            ,{'.','.','.','.','.','.'}};
+        // 2)Imprimir valores
+        for (int i = 0; i < lista.length; i++) {
+            System.out.print(lista [i]+" ");
         }
+        for(int i = 0; i < tablero.length; i++){
+            System.out.println();
+            for (int y = 0; y < tablero[i].length; y++){
+                System.out.print(tablero[i][y]+" ");
+            }
+        }
+    //Pintar barcos 
+        // 3)declarar tablero
+
+        // 4)darle valores al tablero
+        // 5)imprimirlo
+        // 6)printar barcos y cordenadas
     }
 }
